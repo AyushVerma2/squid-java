f9982453ab9385d76a6efa6c84cd36e38958dfb6
josepablo <jose@oceanprotocol.com>
2019-03-25 12:36:47 +0100

    Using publisher address as owner in publicKey

--- src/main/java/com/oceanprotocol/squid/models/DDO.java
+++ src/main/java/com/oceanprotocol/squid/models/DDO.java
@@ -162,7 +162,7 @@ public class DDO extends AbstractModel implements FromJsonToModel {
         this.services.add(metadataService);
 
         this.proof= new Proof(UUID_PROOF_TYPE, publicKey, signature);
-        this.publicKeys.add( new DDO.PublicKey(this.id, ETHEREUM_KEY_TYPE, this.id));
+        this.publicKeys.add( new DDO.PublicKey(this.id, ETHEREUM_KEY_TYPE, publicKey));
     }
 
     @JsonSetter("id")
